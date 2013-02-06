d1784d2d240f4149233e4b0ce7933bda1e4683f4
mbax <matt@phozop.net>
2013-02-06 17:30:13 -0500

    Add date to Kill tracking, also: values

--- src/main/java/org/kitteh/tracker/data/Kill.java
+++ src/main/java/org/kitteh/tracker/data/Kill.java
@@ -2,15 +2,21 @@ package org.kitteh.tracker.data;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
 
 import org.kitteh.tracker.Trackr;
 
 public class Kill extends Data {
     private String killer;
     private String victim;
+    private Timestamp timestamp;
 
     public Kill(String killer, String victim) {
         super(DataType.KILL);
+        this.killer = killer;
+        this.victim = victim;
+        this.timestamp = new Timestamp(new Date().getTime());
     }
 
     @Override
@@ -18,10 +24,11 @@ public class Kill extends Data {
         statement.setString(1, Trackr.getServerName());
         statement.setString(2, this.killer);
         statement.setString(3, this.victim);
+        statement.setTimestamp(4, this.timestamp);
     }
 
     @Override
     protected String getStatementString() {
-        return "INSERT INTO `kills` (`server`,`killer`,`victim`) VALUES (?,?,?);";
+        return "INSERT INTO `kills` (`server`,`killer`,`victim`,`timestamp`) VALUES (?,?,?,?);";
     }
 }
