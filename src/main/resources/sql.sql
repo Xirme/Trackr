d4a905ad1bf486b41e64ca9a27e6fb2e8a9e07f1
mbax <matt@phozop.net>
2013-02-06 19:32:40 -0500

    SQL!

---
+++ src/main/resources/sql.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `kills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `server` varchar(32) NOT NULL,
+  `killer` varchar(16) NOT NULL,
+  `victim` varchar(16) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `server` (`server`),
+  KEY `killer` (`killer`),
+  KEY `victim` (`victim`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `server` varchar(32) NOT NULL,
+  `player` varchar(16) NOT NULL,
+  `start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `duration` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `server` (`server`),
+  KEY `player` (`player`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
