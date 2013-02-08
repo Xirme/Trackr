5961618f4bcf49ba364820632e1439cb6fb1e38b
mbax <matt@phozop.net>
2013-02-08 15:49:42 -0500

    lowercase map for fun

---
+++ src/main/java/org/kitteh/tracker/util/LCMap.java
@@ -0,0 +1,29 @@
+package org.kitteh.tracker.util;
+
+import java.util.Map;
+
+public class LCMap<V> {
+
+    private final Map<String, V> map;
+
+    public LCMap(Map<String, V> starter) {
+        this.map = starter;
+    }
+
+    public boolean containsKey(Object key) {
+        return this.map.containsKey(key.toString().toLowerCase());
+    }
+
+    public V get(Object key) {
+        return this.map.get(key.toString().toLowerCase());
+    }
+
+    public V put(String key, V value) {
+        return this.map.put(key.toLowerCase(), value);
+    }
+
+    public V remove(Object key) {
+        return this.map.remove(key.toString().toLowerCase());
+    }
+
+}
